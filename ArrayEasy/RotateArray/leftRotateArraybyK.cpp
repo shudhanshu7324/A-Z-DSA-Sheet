23aec3faf7fb61bda0a367198b8a4d14ad6b2f4e
Shudhanshu Shree <83982549+shudhanshu7324@users.noreply.github.com>
2024-02-13 16:17:18 +0530

    left rotate the array by d places

---
+++ ArrayEasy/RotateArray/leftRotateArraybyK.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void leftRotatebyK(int arr[],int n,int k){
+    int temp[n];
+
+    for(int i=0;i<n;i++){
+        temp[(k+i)%n] = arr[i];
+    }
+
+    for(int i=0;i<n;i++){
+        arr[i] = temp[i];
+    }
+}
+
+int main(){
+    int arr[] = {3,4,5,6,7,8};
+    int n = sizeof(arr)/sizeof(int);
+    leftRotatebyK(arr,n,3);
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+}
