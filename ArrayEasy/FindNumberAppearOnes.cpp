986554f453bd767e56311976866206e6982b9ffc
Shudhanshu Shree <83982549+shudhanshu7324@users.noreply.github.com>
2024-02-14 19:09:55 +0530

    find the number which is appearing only 1 time in a array

---
+++ ArrayEasy/FindNumberAppearOnes.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int uniqueNumber(int arr[],int n){
+    int ans = 0;
+    
+    for(int i=0;i<n;i++){
+        ans ^= arr[i];
+    }
+
+    return ans;
+}
+
+int main(){
+
+}
