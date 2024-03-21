7b060107307b8a34a9e5c113430ba795585c3289
Shudhanshu Shree <83982549+shudhanshu7324@users.noreply.github.com>
2024-03-22 00:25:45 +0530

    bfs traversal in a graph

--- Graph/Learning'/adjancencyList.cpp
+++ Graph/Learning'/adjancencyList.cpp
@@ -1,13 +1,40 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    int n,m;
-    cin>>n>>m;
-    vector<int> adjList[n+1];
-    for(int i=0;i<m;i++){
-        int u,v;
-        cin>>u>>v;
+vector<int> bfs(vector<int> arr[], int n)
+{
+    int visited[n] = {0};
+    visited[0] = 1;
+    queue<int> q;
+    q.push(0);
+    vector<int> bfs;
+
+    while (!q.empty())
+    {
+        int node = q.front();
+        q.pop();
+        bfs.push_back(node);
+        for (auto it : arr[node])
+        {
+            if (!visited[it])
+            {
+                visited[it] = 1;
+                q.push(it);
+            }
+        }
+    }
+    return bfs;
+}
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    vector<int> adjList[n + 1];
+    for (int i = 0; i < m; i++)
+    {
+        int u, v;
+        cin >> u >> v;
         adjList[u].push_back(v);
         adjList[v].push_back(u);
     }
